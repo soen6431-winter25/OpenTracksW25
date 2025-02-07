97c0e42de7f5235024ca5c83ab6964e34b13f698
ptsshahul <s_thamee@live.concordia.ca>
2025-02-06 19:46:09 -0500

    Make non-static "permissionRequester" transient or serializable.

--- src/main/java/de/dennisguse/opentracks/services/MissingPermissionException.java
+++ src/main/java/de/dennisguse/opentracks/services/MissingPermissionException.java
@@ -2,7 +2,9 @@ package de.dennisguse.opentracks.services;
 
 import de.dennisguse.opentracks.util.PermissionRequester;
 
-public class MissingPermissionException extends RuntimeException {
+public class MissingPermissionException extends RuntimeException implements Serializable {
+
+    private static final long serialVersionUID = 1L; // Recommended for serializable classes
 
     private final PermissionRequester permissionRequester;
 
