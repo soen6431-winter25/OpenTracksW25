9af07da633cf90c4f263bb127caef4ad6e483889
Poorav Panchal <73798517+poorav-panchal@users.noreply.github.com>
2025-03-17 15:54:33 -0400

    Revert "Duplicate Refactor: define a helper method for duplicate code"

--- src/main/java/de/dennisguse/opentracks/data/ContentProviderUtils.java
+++ src/main/java/de/dennisguse/opentracks/data/ContentProviderUtils.java
@@ -310,7 +310,19 @@ public class ContentProviderUtils {
         contentResolver.update(TracksColumns.CONTENT_URI, createContentValues(track), TracksColumns._ID + "=?", new String[]{Long.toString(track.getId().id())});
     }
 
-    private void populateTrackStatistics(ContentValues values, TrackStatistics trackStatistics) {
+    private ContentValues createContentValues(Track track) {
+        ContentValues values = new ContentValues();
+        TrackStatistics trackStatistics = track.getTrackStatistics();
+
+        if (track.getId() != null) {
+            values.put(TracksColumns._ID, track.getId().id());
+        }
+        values.put(TracksColumns.UUID, UUIDUtils.toBytes(track.getUuid()));
+        values.put(TracksColumns.NAME, track.getName());
+        values.put(TracksColumns.DESCRIPTION, track.getDescription());
+        values.put(TracksColumns.ACTIVITY_TYPE, track.getActivityType() != null ? track.getActivityType().getId() : null);
+        values.put(TracksColumns.ACTIVITY_TYPE_LOCALIZED, track.getActivityTypeLocalized());
+        values.put(TracksColumns.STARTTIME_OFFSET, track.getZoneOffset().getTotalSeconds());
         if (trackStatistics.getStartTime() != null) {
             values.put(TracksColumns.STARTTIME, trackStatistics.getStartTime().toEpochMilli());
         }
@@ -327,23 +339,7 @@ public class ContentProviderUtils {
         values.put(TracksColumns.MAX_ALTITUDE, trackStatistics.getMaxAltitude());
         values.put(TracksColumns.ALTITUDE_GAIN, trackStatistics.getTotalAltitudeGain());
         values.put(TracksColumns.ALTITUDE_LOSS, trackStatistics.getTotalAltitudeLoss());
-    }
 
-    private ContentValues createContentValues(Track track) {
-        ContentValues values = new ContentValues();
-        TrackStatistics trackStatistics = track.getTrackStatistics();
-        if (track.getId() != null) {
-            values.put(TracksColumns._ID, track.getId().id());
-        }
-        values.put(TracksColumns.UUID, UUIDUtils.toBytes(track.getUuid()));
-        values.put(TracksColumns.NAME, track.getName());
-        values.put(TracksColumns.DESCRIPTION, track.getDescription());
-        values.put(TracksColumns.ACTIVITY_TYPE, track.getActivityType() != null ? track.getActivityType().getId() : null);
-        values.put(TracksColumns.ACTIVITY_TYPE_LOCALIZED, track.getActivityTypeLocalized());
-        values.put(TracksColumns.STARTTIME_OFFSET, track.getZoneOffset().getTotalSeconds());
-
-        populateTrackStatistics(values, track.getTrackStatistics());
-        
         return values;
     }
 
@@ -353,7 +349,22 @@ public class ContentProviderUtils {
 
     private ContentValues createContentValues(TrackStatistics trackStatistics) {
         ContentValues values = new ContentValues();
-        populateTrackStatistics(values, trackStatistics);
+        if (trackStatistics.getStartTime() != null) {
+            values.put(TracksColumns.STARTTIME, trackStatistics.getStartTime().toEpochMilli());
+        }
+        if (trackStatistics.getStopTime() != null) {
+            values.put(TracksColumns.STOPTIME, trackStatistics.getStopTime().toEpochMilli());
+        }
+        values.put(TracksColumns.TOTALDISTANCE, trackStatistics.getTotalDistance().toM());
+        values.put(TracksColumns.TOTALTIME, trackStatistics.getTotalTime().toMillis());
+        values.put(TracksColumns.MOVINGTIME, trackStatistics.getMovingTime().toMillis());
+        values.put(TracksColumns.AVGSPEED, trackStatistics.getAverageSpeed().toMPS());
+        values.put(TracksColumns.AVGMOVINGSPEED, trackStatistics.getAverageMovingSpeed().toMPS());
+        values.put(TracksColumns.MAXSPEED, trackStatistics.getMaxSpeed().toMPS());
+        values.put(TracksColumns.MIN_ALTITUDE, trackStatistics.getMinAltitude());
+        values.put(TracksColumns.MAX_ALTITUDE, trackStatistics.getMaxAltitude());
+        values.put(TracksColumns.ALTITUDE_GAIN, trackStatistics.getTotalAltitudeGain());
+        values.put(TracksColumns.ALTITUDE_LOSS, trackStatistics.getTotalAltitudeLoss());
         return values;
     }
 
